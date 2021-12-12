5f6135594055fb22c9b23e8b810b081de53a89ab
hugosimao10 <61155165+hugosimao10@users.noreply.github.com>
2021-12-12 19:56:50 +0000

    .

--- Proj3_HugoSimao_DiogoMota/resources/views/partials/initialPage/header.blade.php
+++ Proj3_HugoSimao_DiogoMota/resources/views/partials/initialPage/header.blade.php
@@ -1,6 +1,6 @@
 <header class="masthead">
         <div class="container">
-            <div class="masthead-heading">Bem vindo à nossa empresa</div>
-            <div class="masthead-subheading">Aqui pode solicitar os melhores serviços para arranjos de equipamentos elétricos e para reparações domésticas.</div>
+            <div class="masthead-heading ">Bem vindo à nossa empresa</div>
+            <div class="masthead-subheading ">Aqui pode solicitar os melhores serviços para arranjos de equipamentos elétricos e para reparações domésticas.</div>
         </div>
     </header>
--- Proj3_HugoSimao_DiogoMota/resources/views/partials/initialPage/services.blade.php
+++ Proj3_HugoSimao_DiogoMota/resources/views/partials/initialPage/services.blade.php
@@ -8,7 +8,7 @@
                 <div class="col-md-6">
                     <span class="fa-stack fa-4x">
                         <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                        <i class="fas fa-bolt fa-stack-1x fa-inverse"></i>
                     </span>
                     <h4 class="my-3">Arranjos de equipamentos elétricos</h4>
                     <p class="text-muted">Os nossos técnicos têm acesso a equipamentos de diagnóstico e reparação de última geração, estabelecemos parcerias com empresas ligadas ao ramo da informática e eletrónica para assim fornecer os melhores serviços possíveis. </p>
@@ -16,7 +16,7 @@
                 <div class="col-md-6">
                     <span class="fa-stack fa-4x">
                         <i class="fas fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+                        <i class="fas fa-wrench fa-stack-1x fa-inverse"></i>
                     </span>
                     <h4 class="my-3">Reparações domésticas</h4>
                     <p class="text-muted">A Arranja Tudo tem à sua disposição as melhores soluções para responder as suas necessidades de manutenção do lar, desde tarefas simples como pendurar um quadro até à remodelação integral de uma casa, passando pelos serviços de eletricistas, canalização, Bricolage ou pequenas mudanças, todos eles económicos e a preços de mercado competitivos.</p>
