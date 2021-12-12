913fd0aa3ec776679c06dfaec7ddc7e34d02b4c5
hugosimao10 <61155165+hugosimao10@users.noreply.github.com>
2021-12-12 18:45:36 +0000

    .

---
+++ Proj3_HugoSimao_DiogoMota/resources/views/partials/loginSignUp.blade.php
@@ -0,0 +1,62 @@
+<section class="page-section bg-light" id="loginRegisto">
+    <div class="container">
+        <div class="login-wrap">
+            <div class="login-html">
+                <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Login</label>
+                <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Registar</label>
+                <div class="login-form">
+                    <div class="sign-in-htm">
+                        <div class="group">
+                            <label for="user" class="label">E-mail</label>
+                            <input id="user" type="email" class="input">
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Password</label>
+                            <input id="pass" type="password" class="input" data-type="password">
+                        </div>
+                        <div class="group">
+                            <input type="submit" class="button" value="Sign In">
+                        </div>
+                        <div class="hr"></div>
+                        <div class="foot-lnk">
+                            <a href="#forgot">Esqueceu a password?</a>
+                        </div>
+                    </div>
+                    <div class="sign-up-htm">
+                        <div class="group">
+                            <label for="user" class="label">E-mail</label>
+                            <input id="user" type="email" class="input">
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Password</label>
+                            <input id="pass" type="password" class="input" data-type="password">
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Reescrever Password</label>
+                            <input id="pass" type="password" class="input" data-type="password">
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Primeiro nome</label>
+                            <input id="pass" type="text" class="input">
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Apelido</label>
+                            <input id="pass" type="text" class="input">
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Telemóvel</label>
+                            <input id="pass" type="number" class="input">
+                        </div>
+                        <div class="group">
+                            <label for="pass" class="label">Morada</label>
+                            <input id="pass" type="text" class="input">
+                        </div>
+                        <div class="group">
+                            <input type="submit" class="button" value="Sign Up">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
