c94151ad5396eb7147a15bed3b51043f73acfede
hugosimao10 <61155165+hugosimao10@users.noreply.github.com>
2021-12-12 19:24:36 +0000

    .

--- Proj3_HugoSimao_DiogoMota/resources/views/partials/footer.blade.php
+++ Proj3_HugoSimao_DiogoMota/resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
 
 <footer class="footer py-4">
         <div class="container">
-            <p class="text-center text-uppercase">Hugo Simão, 20752 | Diogo Mota, 20778</p>
+            <b class="text-center text-uppercase">Hugo Simão, 20752 | Diogo Mota, 20778</b>
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
