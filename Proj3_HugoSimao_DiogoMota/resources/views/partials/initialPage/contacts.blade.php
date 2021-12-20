267bd65f985fcb28f203777235db6f46a4d25d0b
hugosimao10 <61155165+hugosimao10@users.noreply.github.com>
2021-12-20 14:48:20 +0000

    .

--- Proj3_HugoSimao_DiogoMota/resources/views/partials/initialPage/contacts.blade.php
+++ Proj3_HugoSimao_DiogoMota/resources/views/partials/initialPage/contacts.blade.php
@@ -1,58 +1,36 @@
 <section class="page-section" id="contact">
-        <div class="container">
-            <div class="text-center">
-                <h2 class="section-heading text-uppercase">Contactos</h2>
-                <h3 class="section-subheading text-muted text-black">Aqui pode encontrar informações sobre como nos contactar</h3>
-            </div>
-            <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                <div class="row align-items-stretch mb-5">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <!-- Name input-->
-                            <input class="form-control" id="name" type="text" placeholder="Your Name *" data-sb-validations="required" />
-                            <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                        </div>
-                        <div class="form-group">
-                            <!-- Email address input-->
-                            <input class="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
-                            <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                            <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                        </div>
-                        <div class="form-group mb-md-0">
-                            <!-- Phone number input-->
-                            <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" data-sb-validations="required" />
-                            <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group form-group-textarea mb-md-0">
-                            <!-- Message input-->
-                            <textarea class="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
-                            <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                        </div>
-                    </div>
-                </div>
-                <!-- Submit success message-->
-                <!---->
-                <!-- This is what your users will see when the form-->
-                <!-- has successfully submitted-->
-                <div class="d-none" id="submitSuccessMessage">
-                    <div class="text-center text-white mb-3">
-                        <div class="fw-bolder">Form submission successful!</div>
-                        To activate this form, sign up at
-                        <br />
-                        <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                    </div>
-                </div>
-                <!-- Submit error message-->
-                <!---->
-                <!-- This is what your users will see when there is-->
-                <!-- an error submitting the form-->
-                <div class="d-none" id="submitErrorMessage">
-                    <div class="text-center text-danger mb-3">Error sending message!</div>
-                </div>
-                <!-- Submit Button-->
-                <div class="text-center"><button class="btn btn-primary btn-xl text-uppercase" id="submitButton" type="submit">Send Message</button></div>
-            </form>
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Contactos</h2>
+            <h3 class="section-subheading text-muted text-black">Aqui pode encontrar informações sobre como nos contactar</h3>
         </div>
-    </section>
+        <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+            <div class="row align-items-stretch mb-5">
+                <div class="col-md-6 backgContact">
+                    <div class="form-group">
+                        <p><b>Morada:</b> Avenida do Atlântico, n.º 644 - Viana do Castelo</p>
+                    </div>
+                    <div class="form-group">
+                        <p><b>Código postal:</b> 4900-348 Viana do Castelo</p>
+                    </div>
+                    <div class="form-group">
+                        <p><b>Telemóvel:</b> 999 444 111</p>
+                    </div>
+                    <div class="form-group">
+                        <p><b>E-mail:</b> arranjatudo@arranjatudo.pt</p>
+                    </div>
+                    <div class="form-group">
+                        <p><b>Horário:</b> Segunda a sábado: 07:00h até 20:00h</p>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group form-group-textarea mb-md-0">
+                        <!-- Message input-->
+                        <textarea class="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
+                        <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
